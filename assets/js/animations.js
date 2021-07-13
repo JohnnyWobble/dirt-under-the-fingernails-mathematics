266272e30388bd9a14abfa4fcbe66102a9152fe2
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2021-07-12 22:08:22 -0400

    Finished sample 1

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -2,11 +2,17 @@ class PS4Q1 {
     size = 5;
     board = "#ps4q1Board";
     active = "I";
-    I = this.getEmptyArray(); 
+    I = this.getEmptyArray();
     U = this.getEmptyArray(1);
     R = this.getEmptyArray();
     t = 0;
     tStep = 1;
+    interval = 500;
+    intervalFn = null;
+
+    constructor() {
+        this.step = this.step.bind(this);
+    }
 
     setupHTML() {
         for (let i = 0; i < this.size * this.size; i++) {
@@ -17,7 +23,7 @@ class PS4Q1 {
     updateHTML() {
         let children = $(this.board).children();
         for (let i = 0; i < this.size * this.size; i++) {
-            let child = $(children[i]); 
+            let child = $(children[i]);
             child.text(this.getValue(i).toFixed(3));
             child.css("background-color", `hsla(${this.getValue(i) * 330 + 90}, 100%, 50%, 0.15)`);
         }
@@ -26,14 +32,19 @@ class PS4Q1 {
     }
 
     start() {
-        this.step();
+        this.intervalFn = window.setInterval(this.step, this.interval);
     }
 
     pause() {
-        console.log("PAUSE");
+        if (this.intervalFn !== null) {
+            window.clearInterval(this.intervalFn);
+            this.intervalFn = null;
+        }
     }
 
     reset() {
+        this.pause();
+
         this.I = this.getEmptyArray();
         this.U = this.getEmptyArray(1);
         this.R = this.getEmptyArray();
@@ -50,31 +61,31 @@ class PS4Q1 {
     }
 
     getValue(index) {
-        return this.getActive()[(index % this.size)][this.size - Math.floor(index/this.size) - 1];
+        return this.getActive()[(index % this.size)][this.size - Math.floor(index / this.size) - 1];
     }
 
     getNewI(x, y) {
-        return this.U[x][y] 
-            * (0.3 
-                * ((x > 0? this.I[x - 1][y] : 0)
-                    + (y > 0? this.I[x][y - 1] : 0)
-                    + (x < this.size - 1? this.I[x + 1][y] : 0) 
-                    + (y < this.size - 1? this.I[x][y + 1] : 0))
+        return this.U[x][y]
+            * (0.3
+                * ((x > 0 ? this.I[x - 1][y] : 0)
+                    + (y > 0 ? this.I[x][y - 1] : 0)
+                    + (x < this.size - 1 ? this.I[x + 1][y] : 0)
+                    + (y < this.size - 1 ? this.I[x][y + 1] : 0))
                 + (0.4 * this.I[x][y]));
     }
 
-    getNewU(x,y) {
+    getNewU(x, y) {
         return this.U[x][y] - this.I[x][y];
     }
 
-    getNewR(x,y) {
+    getNewR(x, y) {
         return this.R[x][y] + this.I[x][y];
     }
 
     step() {
-        let newI = this.getEmptyArray().map((v,i) => v.map((_,j) => this.getNewI(i,j)));
-        let newU = this.getEmptyArray().map((v,i) => v.map((_,j) => this.getNewU(i,j)));
-        let newR = this.getEmptyArray().map((v,i) => v.map((_,j) => this.getNewR(i,j)));
+        let newI = this.getEmptyArray().map((v, i) => v.map((_, j) => this.getNewI(i, j)));
+        let newU = this.getEmptyArray().map((v, i) => v.map((_, j) => this.getNewU(i, j)));
+        let newR = this.getEmptyArray().map((v, i) => v.map((_, j) => this.getNewR(i, j)));
 
         this.I = newI;
         this.U = newU;
@@ -84,7 +95,7 @@ class PS4Q1 {
         this.updateHTML();
     }
 
-    getEmptyArray(fillWith=0) {
+    getEmptyArray(fillWith = 0) {
         return new Array(this.size).fill(fillWith).map(() => new Array(this.size).fill(fillWith));
     }
 
@@ -95,11 +106,11 @@ class PS4Q1 {
         this.active = arrLetter;
         this.updateHTML();
     }
-} 
+}
 
 const ps4q1 = new PS4Q1();
 
-$(function(){
+$(function () {
     ps4q1.setupHTML();
     ps4q1.reset();
 });
